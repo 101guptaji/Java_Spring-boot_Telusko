c3ad19cfdd9e6a9fc0f3d0cbbde46473314eef06
Himanshu Gupta <101guptaji@gmail.com>
2025-05-19 19:10:40 +0530

    Requesting data using Model

--- SpringBootWeb1/src/main/java/org/hg/SpringBootWeb1/controller/HomeController.java
+++ SpringBootWeb1/src/main/java/org/hg/SpringBootWeb1/controller/HomeController.java
@@ -3,6 +3,7 @@ package org.hg.SpringBootWeb1.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,13 +15,21 @@ public class HomeController {
         return "index.jsp";
     }
 
-    @RequestMapping("add")
-    public String add(HttpServletRequest req, HttpSession session) {
+    // @RequestMapping("add")
+    // public String add(HttpServletRequest req, HttpSession session) {
 
-        int num1=Integer.parseInt(req.getParameter("num1"));
-        int num2=Integer.parseInt(req.getParameter("num2"));
-        int result=num1+num2;
-        session.setAttribute("result", result);
+    // int num1=Integer.parseInt(req.getParameter("num1"));
+    // int num2=Integer.parseInt(req.getParameter("num2"));
+    // int result=num1+num2;
+    // session.setAttribute("result", result);
+
+    // return "result.jsp";
+    // }
+
+    @RequestMapping("add")
+    public String add(int num1, int num2, Model model) {
+        int result = num1 + num2;
+        model.addAttribute("result", result);
 
         return "result.jsp";
     }
